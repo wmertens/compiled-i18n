37227aee20a95d1d4b71e51e50212183c45af655
Wout Mertens <Wout.Mertens@gmail.com>
2023-11-03 19:23:38 +0100

    guessLocale: accept empty values
    Nicer when passing accepts header

--- src/guess-locale.ts
+++ src/guess-locale.ts
@@ -6,7 +6,10 @@ import {defaultLocale} from '@i18n/__state'
  * Guess the locale from the Accept-Language header. This can also be used for
  * other strings, but you need to ensure that the string is a valid locale.
  */
-export const guessLocale = (acceptsLanguage: string): Locale => {
+export const guessLocale = (
+	acceptsLanguage: string | null | undefined
+): Locale => {
+	if (!acceptsLanguage) return defaultLocale
 	const locales = acceptsLanguage.split(',').map(l => l.split(';')[0])
 	const locale = locales.find(l => l in localeNames)
 	return locale || defaultLocale
